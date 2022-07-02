e016fc67ef15d3af186c42d199362c78dfdea552
gxsoar <2278926493gx@gmail.com>
2022-07-02 15:07:49 +0800

    修复Unpin的时候只弹出最后一个数据而不在hash_table删除

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -20,7 +20,8 @@ LRUReplacer::LRUReplacer(size_t num_pages) : num_pages_(num_pages) {}
 LRUReplacer::~LRUReplacer() = default;
 
 bool LRUReplacer::Victim(frame_id_t *frame_id) {
-  std::lock_guard<std::mutex> lock(the_mutex_);
+  // std::lock_guard<std::mutex> lock(the_mutex_);
+  std::scoped_lock<std::mutex> lock(the_mutex_);
   if (lru_cache_.empty()) {
     return false;
   }
@@ -32,33 +33,35 @@ bool LRUReplacer::Victim(frame_id_t *frame_id) {
 }
 
 void LRUReplacer::Pin(frame_id_t frame_id) {
-  std::lock_guard<std::mutex> lock(the_mutex_);
-
+  // std::lock_guard<std::mutex> lock(the_mutex_);
+  std::scoped_lock<std::mutex> lcok(the_mutex_);
   // 如果在这个值已经被victim了
   if (to_pos_.count(frame_id) == 0U || lru_cache_.empty()) {
     return;
   }
-
   auto &pos = to_pos_[frame_id];
   lru_cache_.erase(pos);
   to_pos_.erase(frame_id);
 }
 
 void LRUReplacer::Unpin(frame_id_t frame_id) {
-  std::lock_guard<std::mutex> lock(the_mutex_);
+  // std::lock_guard<std::mutex> lock(the_mutex_);
+  std::scoped_lock<std::mutex> lock(the_mutex_);
   // 如果已经存在对应的frame_id
   if (to_pos_.count(frame_id) != 0U) {
     return;
   }
-  if (num_pages_ == lru_cache_.size()) {
-    lru_cache_.pop_back();
+  
+  if (lru_cache_.size() == num_pages_) {
+    frame_id_t frame_id = -1;
+    if (!this->Victim(&frame_id)) return;
   }
+  
   lru_cache_.push_front(frame_id);
   to_pos_[frame_id] = lru_cache_.begin();
 }
 
 size_t LRUReplacer::Size() {
-  std::lock_guard<std::mutex> lock(the_mutex_);
   size_t lru_size = lru_cache_.size();
   return lru_size;
 }
