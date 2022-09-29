f1fa95a3e25ee925e728c9181547a3e7c44495d0
gxsoar <2278926493gx@gmail.com>
2022-09-29 23:29:05 +0800

    begin nested loop join

--- src/execution/nested_loop_join_executor.cpp
+++ src/execution/nested_loop_join_executor.cpp
@@ -17,10 +17,58 @@ namespace bustub {
 NestedLoopJoinExecutor::NestedLoopJoinExecutor(ExecutorContext *exec_ctx, const NestedLoopJoinPlanNode *plan,
                                                std::unique_ptr<AbstractExecutor> &&left_executor,
                                                std::unique_ptr<AbstractExecutor> &&right_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), left_executor_(std::move(left_executor)), right_executor_(std::move(right_executor)) {
+        left_plan_ = plan_->GetLeftPlan();
+        right_plan_ = plan_->GetRightPlan();
+    }
 
-void NestedLoopJoinExecutor::Init() {}
+void NestedLoopJoinExecutor::Init() {
+    if (left_executor_) {
+        // left_table_info_ = left_executor_->GetExecutorContext()->GetCatalog()->GetTable(left_plan_->)
+        left_executor_->Init();
+    }
+    if (right_executor_) {
+        right_executor_->Init();
+    }
+}
 
-bool NestedLoopJoinExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool NestedLoopJoinExecutor::Next(Tuple *tuple, RID *rid) { 
+    const Schema *plan_out_put_schema = plan_->OutputSchema();
+    const Schema *left_out_put_schema = left_plan_->OutputSchema();
+    const Schema *right_out_put_schema = right_plan_->OutputSchema();
+    auto predicate = plan_->Predicate(); 
+    std::vector<std::pair<Tuple, RID>> left_values;
+    std::vector<std::pair<Tuple, RID>> right_values;
+    while(left_executor_->Next(tuple, rid)) {
+        left_values.push_back(std::make_pair(*tuple, *rid));
+    }
+    while(right_executor_->Next(tuple, rid)) {
+        right_values.push_back(std::make_pair(*tuple, *rid));
+    }
+    for (auto &left_val : left_values) {
+        Tuple left_tuple = left_val.first;
+        RID left_rid = left_val.second;
+        for (auto &right_val : right_values) {
+            Tuple right_tuple = right_val.first;
+            RID right_rid = right_val.second;
+            if (predicate == nullptr || 
+                predicate->EvaluateJoin(&left_tuple, left_out_put_schema, &right_tuple, right_out_put_schema).GetAs<bool>()) {
+                std::vector<Value> values;
+                std::vector<Column> left_cols = left_out_put_schema->GetColumns();
+                std::vector<Column> right_cols = right_out_put_schema->GetColumns();
+                for (const auto &l_col : left_cols) {
+                    values.push_back(left_tuple.GetValue(left_out_put_schema, left_out_put_schema->GetColIdx(l_col.GetName())));
+                }
+                for (const auto &r_col : right_cols) {
+                    values.push_back(right_tuple.GetValue(right_out_put_schema, right_out_put_schema->GetColIdx(r_col.GetName())));
+                }
+                *tuple = Tuple(values, plan_out_put_schema);
+                
+            }
+
+        }
+    }
+    return false; 
+}
 
 }  // namespace bustub
--- src/include/execution/executors/nested_loop_join_executor.h
+++ src/include/execution/executors/nested_loop_join_executor.h
@@ -55,6 +55,12 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
  private:
   /** The NestedLoopJoin plan node to be executed. */
   const NestedLoopJoinPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> left_executor_;
+  std::unique_ptr<AbstractExecutor> right_executor_;
+  TableInfo* left_table_info_;
+  TableInfo* right_table_info_;
+  const AbstractPlanNode* left_plan_;
+  const AbstractPlanNode* right_plan_;
 };
 
 }  // namespace bustub
