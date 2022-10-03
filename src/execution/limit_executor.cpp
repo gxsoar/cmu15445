67c981794744dfc472cdeab136f23a581577d91b
gxsoar <2278926493gx@gmail.com>
2022-10-03 20:53:23 +0800

    complete limit

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -16,10 +16,25 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void LimitExecutor::Init() {}
+void LimitExecutor::Init() {
+    if (child_executor_) {
+        child_executor_->Init();
+    }
+    limits_ = 0;
+}
 
-bool LimitExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
+    auto plan_limit = plan_->GetLimit(); 
+    if (limits_ == plan_limit) {
+        return false;
+    }
+    while(child_executor_->Next(tuple, rid) && limits_ < plan_limit) {
+        ++limits_;
+        return true;
+    }
+    return false; 
+}
 
 }  // namespace bustub
